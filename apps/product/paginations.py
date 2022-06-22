905f63994666b8f8b25d6b14a0306e2939f25093
Elina <babulekh010@gmail.com>
2022-06-22 11:55:27 +0600

    paginations modified

--- apps/product/paginations.py
+++ apps/product/paginations.py
@@ -2,6 +2,6 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class ProductPagination(PageNumberPagination):
-    page_size = 20
+    page_size = 8
     page_query_param = 'page'
     max_page_size = 1000
